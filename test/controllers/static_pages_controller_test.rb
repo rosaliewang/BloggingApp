69f8b2fbf5bcacc48554b3c2cc99726fd377fd4c
Yuchen Wang <yuchen.wang2012@gmail.com>
2015-01-18 15:43:11 -0500

    Modify helper

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -10,13 +10,19 @@ class StaticPagesControllerTest < ActionController::TestCase
   test "should get help" do
     get :help
     assert_response :success
-    assert_select "title", "Blog App"
+    assert_select "title", "Help | Blog App"
   end
 
   test "should get about" do
     get :about
     assert_response :success
-    assert_select "title", "Blog App"
+    assert_select "title", "About | Blog App"
+  end
+
+  test "should get contact" do
+    get :contact
+    assert_response :success
+    assert_select "title", "Contact | Blog App"
   end
 
 end
